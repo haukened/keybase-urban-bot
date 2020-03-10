423e5a9ba6b248053765b1496aedf49187b1152a
David Haukeness <david@hauken.us>
2020-03-10 23:10:48 +0000

    moved parseArgs to utils

--- utils.go
+++ utils.go
@@ -9,3 +9,30 @@ func Debug(s string, a ...interface{}) {
 		log.Printf(s, a...)
 	}
 }
+
+func parseArgs(args []string) error {
+	// first check for command line flags
+	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
+	flags.BoolVar(&debug, "debug", false, "enables command debugging")
+	if err := flags.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	// then check the env vars
+	envDebug := os.Getenv("BOT_DEBUG")
+	if envDebug != "" {
+		ret, err := strconv.ParseBool(envDebug)
+		if err != nil {
+			return err
+		}
+
+		// if flag was false but env is true, set debug
+		if debug == false && ret == true {
+			debug = true
+		}
+	}
+	if debug {
+		log.Println("Debugging enabled.")
+	}
+	return nil
+}
